2b2b1f1dfd4a90785d9f1879b092d66de44c283a
gaurav <gauravanchan068@gmail.com>
2024-12-06 13:12:14 +0530

    flick-vault

---
+++ data.js
@@ -0,0 +1,307 @@
+export const movie=
+  
+[   {
+  id: 533535,
+  title: "Deadpool & Wolverine",
+  release_date: "2024-07-24",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
+  category: "Action",
+},
+{
+  id: 718821,
+  title: "Twisters",
+  release_date: "2024-07-10",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/58D6ZAvOKxlHjyX9S8qNKSBE9Y.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/pjnD08FlMAIXsfOLKQbvmO0f0MD.jpg",
+  category: "Thriller",
+},
+{
+  id: 1022789,
+  title: "Inside Out 2",
+  release_date: "2024-06-11",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
+  category: "Animation",
+},
+{
+  id: 519182,
+  title: "Despicable Me 4",
+  release_date: "2024-06-20",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/lgkPzcOSnTvjeMnuFzozRO5HHw1.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/wWba3TaojhK7NdycRhoQpsG0FaH.jpg",
+  category: "Animation",
+},
+{
+  id: 573435,
+  title: "Bad Boys: Ride or Die",
+  release_date: "2024-06-05",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/3q01ACG0MWm0DekhvkPFCXyPZSu.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/oGythE98MYleE6mZlGs5oBGkux1.jpg",
+  category: "Action",
+},
+{
+  id: 1160018,
+  title: "Kill",
+  release_date: "2024-07-03",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/okVLmXL5y18dfN2R4ufMZEGaeCd.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/m2zXTuNPkywdYLyWlVyJZW2QOJH.jpg",
+  category: "Thriller",
+},
+{
+  id: 762441,
+  title: "A Quiet Place: Day One",
+  release_date: "2024-06-26",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/2RVcJbWFmICRDsVxRI8F5xRmRsK.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/hU42CRk14JuPEdqZG3AWmagiPAP.jpg",
+  category: "Horror",
+},
+{
+  id: 831815,
+  title: "Saving Bikini Bottom: The Sandy Cheeks Movie",
+  release_date: "2024-08-01",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/hdFIdXwS8FSN2wIsuotjW1mshI0.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/30YnfZdMNIV7noWLdvmcJS0cbnQ.jpg",
+  category: "Animation",
+},
+{
+  id: 1079091,
+  title: "It Ends with Us",
+  release_date: "2024-08-07",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/8yPSYhooj8nyBbmV3GVdLDwuE7e.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/AjV6jFJ2YFIluYo4GQf13AA1tqu.jpg",
+  category: "Drama",
+},
+{
+  id: 945961,
+  title: "Alien: Romulus",
+  release_date: "2024-08-13",
+  backdrop_path:
+    "https://image.tmdb.org/t/p/original/9SSEUrSqhljBMzRe4aBTh17rUaC.jpg",
+  poster_path:
+    "https://image.tmdb.org/t/p/original/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg",
+  category: "Sci-Fi",
+},
+{
+  "id": 945962,
+  "title": "Galactic Odyssey",
+  "release_date": "2024-09-10",
+  "backdrop_path": "https://image.tmdb.org/t/p/original/8RCfX8l4mlJ1u2dZgqxHUjcOTsK.jpg",
+  "poster_path": "https://image.tmdb.org/t/p/original/wcKFYIiVDvRURrzglV9kGu7fpfY.jpg",
+  "category": "Sci-Fi"
+},
+{
+  "id": 945963,
+  "title": "Cosmic Anomaly",
+  "release_date": "2024-07-22",
+  "backdrop_path": "https://image.tmdb.org/t/p/original/qz5KtEOXtNDS0rMoTS34zpe9SY6.jpg",
+  "poster_path": "https://image.tmdb.org/t/p/original/d5NXSklXo0qyIYkgV94XAgMIckC.jpg",
+  "category": "Sci-Fi"
+},
+{
+  "id": 945964,
+  "title": "Stellar Frontiers",
+  "release_date": "2024-10-05",
+  "backdrop_path": "https://image.tmdb.org/t/p/original/kGzFbGhp99zva6oZODW5atUtnqi.jpg",
+  "poster_path": "https://image.tmdb.org/t/p/original/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg",
+  "category": "Sci-Fi"
+},
+{
+  "id": 945965,
+  "title": "Nebula Chronicles",
+  "release_date": "2024-11-15",
+  "backdrop_path": "https://image.tmdb.org/t/p/original/cgeXb2O7eMy1P7iRXl8RQ1CnVbG.jpg",
+  "poster_path": "https://image.tmdb.org/t/p/original/qEFfVKvMssWbL7zDBIH0Y0rF8W8.jpg",
+  "category": "Sci-Fi"
+},
+{
+  "id": 945966,
+  "title": "Eclipse Protocol",
+  "release_date": "2024-12-20",
+  "backdrop_path": "https://image.tmdb.org/t/p/original/rLo6AlqXT8TmWeDbWyRj8nDqczk.jpg",
+  "poster_path": "https://image.tmdb.org/t/p/original/2EXgNgHKBvZXXzNLPQA9Exe4Om3.jpg",
+  "category": "Sci-Fi"
+},
+  {
+    "id": 945977,
+    "title": "Interstellar",
+    "release_date": "2014-11-07",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945978,
+    "title": "Dune",
+    "release_date": "2021-10-22",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/8A2Of7xrX1E6AkQ4h8P7Owa8P7Q.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/d5NXSklXo0qyIYkgV94XAgMIckC.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945979,
+    "title": "The Martian",
+    "release_date": "2015-10-02",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/xlJbNT3MP4BxNJSrvfyoA9qYURU.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/5BHuvQ6p9kfc091Z8RiFNhCwL4b.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945980,
+    "title": "Gravity",
+    "release_date": "2013-10-04",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/k21nzbJkAAgbK7AUVXG00vUf43w.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/uC6TTUhPpQCmgldGyYveKRAu8JN.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945981,
+    "title": "Blade Runner 2049",
+    "release_date": "2017-10-06",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/4sFt0HXsJU51dWVFu1GiufUb7c0.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/a4I481szRmycyreQTLkU06Dx0Tj.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945982,
+    "title": "Avatar",
+    "release_date": "2009-12-18",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/amYkOxCwHiVTFKendcIW0rSrRlU.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/kyeqWdyUXW608qlYkRqosgbbJyK.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945983,
+    "title": "Star Wars: The Force Awakens",
+    "release_date": "2015-12-18",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/kvXLZqY0Ngl1XSw7EaMQO0C1CCj.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/wqnLdwVXoBjKibFRR5U3y0aDUhs.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945984,
+    "title": "Prometheus",
+    "release_date": "2012-06-08",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/8zdAJN7zQllCyT9yUksGk8r8Ytq.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/9h6Zv0UFS4WbZAd4hoD2ImEM4t7.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945985,
+    "title": "Arrival",
+    "release_date": "2016-11-11",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/yIZ1xendyqKvY3FGeeUYUd5X9Mm.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/4Iu5f2nv7huqvuYkmZvSPOtbFjs.jpg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 945986,
+    "title": "Edge of Tomorrow",
+    "release_date": "2014-06-06",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/mr6LJdHHiKjfJUyMv9IOkfzEzIH.jpg",
+    "poster_path": "https://image.tmdb.org/t/p/original/uUHvlkLavotfGsNtosDy8ShsIhk.jpg",
+    "category": "Sci-Fi"
+  },
+    {
+      "id": 945987,
+      "title": "Oblivion",
+      "release_date": "2013-04-19",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/srl8J7E2FwxXGkQX6y5i1ALZrcG.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/r0qnHtktwb1JoJG3jz01yYn6p8g.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945988,
+      "title": "Elysium",
+      "release_date": "2013-08-09",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/6dVckGkMOTVNPbESuU3Ie9Guvjc.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/tuZhD61LVfiXXDbpGJdL2NGpzfA.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945989,
+      "title": "Pacific Rim",
+      "release_date": "2013-07-12",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/1R9vu8OktwD52yrREyMpsGNW1W6.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/s80DU3eEYJvQSOUl6PgKrNpUXrG.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945990,
+      "title": "War of the Worlds",
+      "release_date": "2005-06-29",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/3TVoO0PqlpDDhFEuU7QojIRRX4S.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/6zHyk5QyxvDYgBSd3GYeNU9XVj4.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945991,
+      "title": "Contact",
+      "release_date": "1997-07-11",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/wk7jblYkb2slCRojVQbnMxlGR8B.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/ya5y0ryty8u47c5DRZJHZK4S6dl.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945992,
+      "title": "Minority Report",
+      "release_date": "2002-06-21",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/utlo9uOw4fBLPy4CmK6zQYfbmsx.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/ccqpHq5tk5W4ymbSbuoy4uYOxFI.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945993,
+      "title": "Moon",
+      "release_date": "2009-06-12",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/9TR2Uj4dxZge4ZlM5VRY1wrqIPc.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/f3nLR1uRnr4AIKJCQMRL9KeN9sX.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945994,
+      "title": "Children of Men",
+      "release_date": "2006-09-22",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/xOdNRU4nF6zEOp7uXH3y2WEqyz1.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/yokJq71WZUl8oUwbkyJ0UYtdXLZ.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945995,
+      "title": "The Fifth Element",
+      "release_date": "1997-05-09",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/6rRMad3CB8y2ibVY5ZSr6Q3acFf.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/fPtlCO1yQtnoLHOwKtWz7db6RGU.jpg",
+      "category": "Sci-Fi"
+    },
+    {
+      "id": 945996,
+      "title": "Alita: Battle Angel",
+      "release_date": "2019-02-14",
+      "backdrop_path": "https://image.tmdb.org/t/p/original/eQuGWn2bsuI04uzNTtiqp1MtiPQ.jpg",
+      "poster_path": "https://image.tmdb.org/t/p/original/xRWht48C2V8XNfzvPehyClOvDni.jpg",
+      "category": "Sci-Fi"
+    }
+  
+  
+
+
+  ]
